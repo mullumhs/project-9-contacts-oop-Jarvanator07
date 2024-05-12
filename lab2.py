deb9d93af4614a9ba6e9e07ba9af30bec31aaf29
Jarvanator07 <140124249+Jarvanator07@users.noreply.github.com>
2024-05-13 09:49:35 +1000

    jkvghkff

--- lab2.py
+++ lab2.py
@@ -11,13 +11,20 @@
 # This class should have an initialiser with attributes for name, phone_number, and email.
 # Add a class attribute to keep track of the total number of contacts.
 
-
+class Contact:
+    total_contacts = 0
+    def __init__(self, name, phone_number):
+        self.name = name
+        self.phone_number = phone_number
+        Contact.total_contacts += 1
 
 # Create at least two instances of the Contact class with different data.
 
-
+contact1 = Contact("John", "0797979797")
+contact2 = Contact("Jane", "0787878787")
 
 # Write code that prints out the details of each contact you have created.
 
-
+print(contact1.name, contact1.phone_number)
+print(contact2.name, contact2.phone_number)
 
---
+++ tester.py
@@ -0,0 +1,8 @@
+class Bicycle:
+    def __init__(self, color, style):
+        self.color = color
+        self.style = style
+
+my_bike = Bicycle("red", "road")
+print(my_bike.color)
+print(my_bike.style)
